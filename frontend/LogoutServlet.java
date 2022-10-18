f10975671a9a1e404068e46f27eb2fc9ee561929
mathus256 <114354025+mathus256@users.noreply.github.com>
2022-10-18 13:37:34 -0400

    for the post when the logout button is pressed

---
+++ frontend/LogoutServlet.java
@@ -0,0 +1,24 @@
+package test;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LogoutServlet extends HttpServlet {
+	
+	//this method is for html post stuff 
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		// New location to be redirected
+		String site = new String("base.html");
+		response.setStatus(response.SC_MOVED_TEMPORARILY);
+		response.setHeader("Location", site); 
+	}
+	
+	//this is what get methods look like
+	public void doGet() {
+		
+	}
+
+}
