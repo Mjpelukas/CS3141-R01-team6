c02011b490a2b9bc269d3ca5617393f75206d90d
mathus256 <114354025+mathus256@users.noreply.github.com>
2022-10-21 11:39:33 -0400

    Penoyar, 10/21: adds new account data to database
    Is actually going to a .txt file = THIS METHOD OF DOING IT IS TEMPORARY

---
+++ frontend/AccountCreationServlet.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AccountCreationServlet extends HttpServlet {
+	
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		String username = request.getParameter("create_username");
+		String password = request.getParameter("create_password");
+		
+		Login log = new Login();
+		
+		if (log.addAccountToFile(username, password) == 0) {
+			//new location to be redirected
+			//location goes to "loggedIn.html" (a new web page)
+			// (if we wanted to keep the .php file we'd do "name.php" instead)
+			String site = new String("loggedIn.html");
+			response.setStatus(response.SC_MOVED_TEMPORARILY);
+			response.setHeader("Location", site); 			
+		} else {
+			PrintWriter out = response.getWriter();
+			out.println("login incorrect");			
+		}
+	}
+
+}
