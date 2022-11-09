489423ec1378efb9d690cb91f6cbb8410be6685e
mareuchl <114351437+mareuchl@users.noreply.github.com>
2022-11-09 13:07:11 -0500

    Update GetFlashcards.java

--- Quizzler/src/GetFlashcards.java
+++ Quizzler/src/GetFlashcards.java
@@ -16,44 +16,64 @@ public class GetFlashcards extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		
+		// Get's username from session context
 		String username = (String) getServletContext().getAttribute("username");
+		
+		// Gets the requested set name from the website submit parameter
 		String setName = request.getParameter("set_name");
 
+		// Sets the session set name so that it can be accessed by other methods
 		getServletContext().setAttribute("memberSet", setName);
-		System.out.println("get setName: " + setName);
+		
+		// Testing print statement 
+		//System.out.println("get setName: " + setName);
+		
+		// Gets the terms and definitions from the database in an array list of string arrays
 		Database database = new Database();
 		database.connect();
 		ArrayList<String[]> terms = database.viewFlashcardSet(setName, username);
+		database.disconnect();
 		
+		// Sets the terms attribute to the arraylist of string arrays so that it can be accessed by teh front end
 		request.setAttribute("terms", terms);
 		try {
+			// Sends to the next website. The request and response are forwarded so that the attributes stay
+			// (I'm only pretty sure that's how that works)
 			request.getRequestDispatcher("FlashcardView.jsp").forward(request, response);
-		} catch (Exception e) {
-		}
-		database.disconnect();
+		} catch (Exception e) {}
 	}
 	
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
 	 *      response)
+	 * This method creates flashcards or is supposed, it is currently a little broken I believe
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		//doGet(request, response);
 		//String setName = request.getParameter("set_name");
+		
+		// This gets the term, definition, setname, and username so that it has all the information needed to to an sql insert
 		String term = request.getParameter("term");
 		String definition = request.getParameter("definition");
 		String memberSet = (String) getServletContext().getAttribute("memberSet");
-		System.out.println("setName: " + memberSet);
 		String username = (String) getServletContext().getAttribute("username");
 		
-		System.out.println(term);
+		//Testing
+		//System.out.println("setName: " + memberSet);
+		//System.out.println(term);
 		
+		//Calls database.java method to create the flashcard with a mysql insert statement
 		Database database = new Database();
 		database.connect();
 		database.createFlashcard(term, definition, memberSet, username);
 		
-		
+		/* 
+		 * This might not be necessary. This basically outputs a success page with a back button.
+		 * The reason we have this is so that the back button can do a get action to update the flashcards page.
+		 * There's a probably a way to avoid this and just update the flashcards page and return there, but I couldn't figure it out.
+		 */
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
 		
