5c98cd9c20cd005c0aa6b2cc407cd5a167f45442
maxim <maxim@141.219.247.66>
2022-11-16 13:20:37 -0500

    Updated sql tables

--- Quizzler/WebContent/Sql/tables.sql
+++ Quizzler/WebContent/Sql/tables.sql
@@ -1,73 +1,89 @@
+use mareuchl;
 /* Drop tables if they already exist in memory. */
-drop table if exists Users;
-drop table if exists FlashcardSets;
-drop table if exists Flashcards;
-drop table if exists Quizzes;
-drop table if exists QuizQuestions;
 drop table if exists Mastery;
+drop table if exists QuizQuestions;
+drop table if exists Quizzes;
+drop table if exists Flashcards;
+drop table if exists FlashcardSets;
+drop table if exists Users;
 
 /* Create Table SQL Statements */
 create table Users
 (
     username varchar(30) primary key,
-    email varchar(20) not null,
-    user_password varchar(256) not null,
-    isLoggedIn int default 0
+    email varchar(20),
+    user_password varchar(256) not null
 );
 
 create table FlashcardSets
 (
-    setName varchar(30) primary key,
+    setName varchar(30),
     setOwner varchar(30),
-    course varchar(10) not null,
-    isPublic int default 0,
+    course varchar(10),
+    isPublic tinyint default 0,
     mastery int default 0,
+    primary key(setName, setOwner),
     foreign key(setOwner) references Users(username)
+    on delete cascade
+    on update cascade
 );
 
 create table Flashcards
 (
-    term varchar(50) primary key,
+    term varchar(50),
     term_definition varchar(256) not null,
     setName varchar(30),
-    foreign key(setName) references FlashcardSets(setName)
+    setOwner varchar(30),
+    primary key(term, setName, setOwner),
+    foreign key(setName, setOwner) references FlashcardSets(setName, setOwner)
+    on delete cascade
+    on update cascade
 );
 
 create table Quizzes
 (
-    quizName varchar(30) primary key,
+    quizName varchar(30),
     quizOwner varchar(30),
     course varchar(10) not null,
     isPublic int default 0,
+    primary key(quizName, quizOwner),
     foreign key(quizOwner) references Users(username)
+    on delete cascade
+    on update cascade
 );
 
 -- IDENTITY is the equivalent of AUTO_INCREMENT
 create table QuizQuestions
 (
-    question int primary key IDENTITY,
+	quizOwner varchar(30),
     quizName varchar(30),
+    question int,
     questionType varchar(20) not null,
     prompt varchar(256) not null,
-    answer varchar(256) not null,
+    answer char(1) not null,
     choiceA varchar(256) not null,
-    choiceB varchar(256) not null,
-    choiceC varchar(256) not null,
-    choiceD varchar(256) not null,
-    foreign key(quizName) references Quizzes(quizName)
+    choiceB varchar(256),
+    choiceC varchar(256),
+    choiceD varchar(256),
+    primary key(quizOwner, quizName, question),
+    foreign key(quizName, quizOwner) references Quizzes(quizName, quizOwner)
+    on delete cascade
+    on update cascade
 );
+
 
 create table Mastery
 (
-    username varchar(30),
+    quizOwner varchar(30),
     quizName varchar(30),
     question int,
     correctGuesses int default 0,
     totalGuesses int default 0,
-    primary key(username, quizName, quizQuestion),
-    foreign key(username) references Users(username),
-    foreign key(quizName, question) references QuizQuestions(quizName, question)
+    primary key(quizOwner, quizName, question),
+    foreign key(quizName, quizOwner, question) references QuizQuestions(quizName, quizOwner, question)
+    on delete cascade
+    on update cascade
 );
 
-/* Create Index SQL Statements */
-CREATE INDEX usernameIndex ON TABLE Mastery(username);
+/* Create Index SQL Statements 
+CREATE INDEX usernameIndex ON TABLE Mastery(username);*/
